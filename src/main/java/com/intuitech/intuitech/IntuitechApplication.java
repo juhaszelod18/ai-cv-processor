4ac4e6cf91a886f4f447b9d3cb87a0fbfbd53dbb
juhaszelod18 <juhaszelod18@gmail.com>
2026-02-18 10:26:59 +0100

    init

---
+++ src/main/java/com/intuitech/intuitech/IntuitechApplication.java
@@ -0,0 +1,13 @@
+package com.intuitech.intuitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntuitechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntuitechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/intuitech/intuitech/IntuitechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuitech.intuitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntuitechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
